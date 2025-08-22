3bdcd5c6f61ebbf8757e72dcb37ee5cc16755af9
iwanaga1 <k024g1001@g.neec.ac.jp>
2025-08-22 10:57:56 +0900

    プロジェクト ファイルを追加します。

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+template <typename Type>
+
+char Min(Type a, Type b) {
+
+	///関数テンプレート
+	if (a > b) {
+		return static_cast<Type>(b);
+	}
+	else {
+		return static_cast<Type> (a);
+	}
+}
+
+
+
+int main() {
+
+
+	//計算と結果出力
+	printf("%d\n", Min<int>(128, 256));
+	printf("%f\n", Min<float>(52.5f, 55.0f));
+	printf("%lf\n", Min<double>(3.14159265, 2.7182818281));
+
+	return 0;
+}
